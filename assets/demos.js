b2be528c4fc278e859d36f13b2874d135d941787
Tresre <58750285+Tresre@users.noreply.github.com>
2021-05-21 14:20:59 -0400

    Update demos.js

--- assets/demos.js
+++ assets/demos.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
   var typed = new Typed('#typed', {
     stringsElement: '#typed-strings',
-    typeSpeed: 20,
-    backSpeed: 20,
+    typeSpeed: 10,
+    backSpeed: 10,
     startDelay: 1000,
     loop: true,
     loopCount: Infinity,
